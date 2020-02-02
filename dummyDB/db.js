c8003073881f32936dff874c4148f8aaf4e1d939
Ali Nadjar <ali.nadjar95@gmail.com>
2020-02-02 12:51:49 +0330

    Cache DB added

---
+++ dummyDB/db.js
@@ -0,0 +1,16 @@
+
+const todos = [
+    {
+        id: 1,
+        title: 'lunch',
+        description: 'Go for lunch by 2 PM'
+    },
+    {
+        id: 2,
+        title: 'Swimming Pool',
+        description: 'At 8 PM'
+    }
+];
+
+
+module.exports = todos;
