ba19c911892f226f5d6f868bdf840f2a35d96795
Ali Nadjar <ali.nadjar95@gmail.com>
2020-02-17 11:25:30 +0330

    before passport

---
+++ middlewares/Auth_middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = (req, res, next) => {    
+
+    const token = req.header('x-auth-token');    
+
+    if(!token) 
+        return res.status(401).send('Access Denied, No token provided');
+
+    try{
+        // const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        const decoded = jwt.verify(token, config.get('SECRET'));
+        console.log('TOKEN DECODED: ' + JSON.stringify(decoded));
+        req.user = decoded; // this decoded is the payload object embedded in the token upon creation
+        next();
+    }
+    catch (ex) {
+        console.log(ex);
+        res.status(400).send('Invalid Token');
+    }
+
+}
